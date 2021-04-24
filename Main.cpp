a87e3b595c1442093b1ee5937dd661369bdb32ae
cianjinks <cjinks99@gmail.com>
2021-04-24 18:37:16 +0100

    Added ImGUI support

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,10 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
+#define IMGUI_IMPL_OPENGL_LOADER_GLAD
+#include "backends/imgui_impl_opengl3.cpp"
+#include "backends/imgui_impl_glfw.cpp"
+
 int main()
 {
     /* Initialize the library */
@@ -26,13 +30,26 @@ int main()
         return -1;
     }
 
-    glClearColor(1.0f, 0.25f, 0.0f, 0.0f);
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 410");
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(1.0f, 0.25f, 0.0f, 0.0f);
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::ShowDemoWindow();
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -40,6 +57,10 @@ int main()
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     glfwTerminate();
     return 0;
